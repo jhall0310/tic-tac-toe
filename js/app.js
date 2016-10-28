7dd60c6020e5b263338cf97d074458e8eb499ee0
jhall0310 <jonathanphall@me.com>
2016-10-28 15:12:31 -0700

    Game Board Created divs assigned

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 // wait for the DOM to finish loading
+console.log('JavaScript ready ')
 $(document).ready(function() {
   // all code to manipulate the DOM
-  // goes inside this function
+  // }goes inside this function
+// var squares = ['aOne', 'aTwo', 'aThree', 'bOne', 'bTwo', 'bThree','cOne', 'cTwo','cThree'];
 
-});
+  $('.box').on("click", function handleClick(event){
+    $('<p>X</p>').appendTo('this#id');
+    })
+
+    $('.box').on("dblclick", function handleClick(event){
+      $('<p>O</p>').appendTo('.box');
+      })
+
+})
