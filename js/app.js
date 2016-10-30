f1bcb01280e540f0314b298530f947f0bf7e47b8
jhall0310 <jonathanphall@me.com>
2016-10-30 08:46:14 -0700

    horizontal rows fixed

--- js/app.js
+++ js/app.js
@@ -3,14 +3,45 @@ console.log('JavaScript ready ')
 $(document).ready(function() {
   // all code to manipulate the DOM
   // }goes inside this function
-// var squares = ['aOne', 'aTwo', 'aThree', 'bOne', 'bTwo', 'bThree','cOne', 'cTwo','cThree'];
 
-  $('.box').on("click", function handleClick(event){
-    $('<p>X</p>').appendTo('this#id');
-    })
+  var x = true;
+  // wait for the DOM to finish loading
+  $(document).ready(function() {
+   // all code to manipulate the DOM
+   // goes inside this function
+   var cells = $(".box");
+   $(cells).on('click', function(){
+     putXorO(this);
+     isGameOver();
+   });
+  });
 
-    $('.box').on("dblclick", function handleClick(event){
-      $('<p>O</p>').appendTo('.box');
-      })
+  function putXorO(l){
+   if(x){
+     $(l).html("<h1>X</h1>");
+     $(l)
+     x = !x;
+   }
+   else {
+     $(l).html("<h1>O</h1>");
+     x = !x;
+   }
+  }
+
+  function isGameOver(){
+   if(($('#cell0').text() === $('#cell1').text()) && ($('#cell1').text()=== $('#cell2').text()) && ($('#cell0').text() === $('#cell2').text())){
+     alert("VICTORY");
+   }
+   if (($('#cell3').text() === $('#cell4').text()) && ($('#cell4').text()=== $('#cell5').text()) && ($('#cell3').text() === $('#cell5').text())){
+     alert("VICTORY");
+   }
+   if (($('#cell6').text() === $('#cell7').text()) && ($('#cell7').text()=== $('#cell8').text()) && ($('#cell6').text() === $('#cell8').text())){
+     alert("VICTORY");
+   }
+  }
+
+
+  console.log('ready');
+
 
 })
